e3607b137a6b8a774e0d057f114ee601b59ec659
wjdtj9656 <wjdtj9656@naver.com>
2019-08-24 09:45:29 +0900

    11021 problem

--- 11021 problem/Main.java
+++ 11021 problem/Main.java
@@ -1,13 +1,18 @@
 
 import java.io.*;
 import java.util.*;
-
+/*
+ * backjoon 11021 problem
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ * https://www.acmicpc.net/problem/11021
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int num=sc.nextInt();
 		int num2[]=new int[num];
+		
 	for(int i=1;i<num2.length+1;i++){
 		int a=sc.nextInt();
 		int b=sc.nextInt();
