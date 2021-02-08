a285763c74ccddec5e41a6521c7a194b06c371aa
wjdtj9656 <wjdtj9656@naver.com>
2021-02-09 02:37:29 +0900

    2021/02/09

---
+++ 1010 problem/Main.java
@@ -0,0 +1,46 @@
+package javaBaekjoon;
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+
+	/*
+	 * 1010 problem 다리 놓기
+	재원이는 한 도시의 시장이 되었다. 이 도시에는 도시를 동쪽과 서쪽으로 나누는 큰 일직선 모양의 강이 흐르고 있다. 하지만 재원이는 다리가 없어서 시민들이 강을 건너는데 큰 불편을 겪고 있음을 알고 다리를 짓기로 결심하였다. 강 주변에서 다리를 짓기에 적합한 곳을 사이트라고 한다. 재원이는 강 주변을 면밀히 조사해 본 결과 강의 서쪽에는 N개의 사이트가 있고 동쪽에는 M개의 사이트가 있다는 것을 알았다. (N ≤ M)
+	재원이는 서쪽의 사이트와 동쪽의 사이트를 다리로 연결하려고 한다. (이때 한 사이트에는 최대 한 개의 다리만 연결될 수 있다.) 재원이는 다리를 최대한 많이 지으려고 하기 때문에 서쪽의 사이트 개수만큼 (N개) 다리를 지으려고 한다. 다리끼리는 서로 겹쳐질 수 없다고 할 때 다리를 지을 수 있는 경우의 수를 구하는 프로그램을 작성하라.
+	 */
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		int arr[][] = new int[30][30];//0<N<=M<30 이므로
+		int N = 0;
+		int M = 0;
+		
+		for(int i=0; i<30; i++) {
+			//사이트 수가 같을때는 1가지 방법뿐이므로
+			arr[i][i] = 1;
+			//왼쪽 사이트가 1개이고, 오른쪽 사이트가 하나씩 늘어날때는 가짓수가 하나씩 늘어남.
+			arr[0][i] = i+1;
+		}
+		for(int i=1; i<30; i++) {
+			for(int j=i+1; j<30; j++) {
+				arr[i][j] = arr[i][j-1] + arr[i-1][j-1];
+			}
+		}
+		for(int i=0; i<num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			//arr[0][0]이 N=1 M=1를 의미 하므로 -1을 하여 출력한다.
+			System.out.println(arr[N-1][M-1]);
+		}
+		bw.flush();
+		bw.close();
+	}
+}
