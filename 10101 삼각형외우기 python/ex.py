1c2e8f9e1a05324227ce4c16c2490fecf1e8e574
wjdtj9656 <wjdtj9656@naver.com>
2021-07-27 15:56:51 +0900

    10101 python

---
+++ 10101 삼각형외우기 python/ex.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if(a==b==c):
+    print('Equilateral')
+elif(a+b+c == 180 and ((a==b) or (b==c) or(a==c))):
+    print('Isosceles')
+elif(a+b+c ==180):
+    print('Scalene')
+else:
+    print('Error')
