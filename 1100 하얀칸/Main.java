49c358d650263b6092dcfa280f70cd93b7b0be99
wjdtj9656 <wjdtj9656@naver.com>
2021-10-23 21:28:56 +0900

    14888 problem

---
+++ 1100 하얀칸/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.IntStream;
+import java.awt.Point;
+import java.io.*;
+	
+	class Main {
+		public static void main(String args[]) throws IOException {
+	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    	char map[][] = new char[8][8];
+	    	int result = 0;
+	    	
+	    	
+	    	for(int i=0; i<8; i++) {
+	    		String s = br.readLine();
+	    		for(int j=0; j<8; j++) {
+	    			map[i][j] = s.charAt(j);
+	    			if(i%2==0) {
+	    				if(j%2==0) {
+	    					if(map[i][j] == 'F') result++;
+	    				}
+	    			}
+	    			else if(i%2==1) {
+	    				if(j%2==1) {
+	    					if(map[i][j] == 'F') result++;
+	    				}
+	    			}
+	    		}
+	    	}
+	    	bw.write(String.valueOf(result));
+	    	br.close();
+	    	bw.flush();
+	    	bw.close();
+	    }
+	}
