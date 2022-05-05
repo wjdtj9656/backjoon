84541cbf7a82fb027a12403c104f3595f37888ef
wjdtj9656 <wjdtj9656@naver.com>
2022-05-05 18:15:48 +0900

    java

--- 2504 괄호의 값/Main.java
+++ 2504 괄호의 값/Main.java
@@ -1,79 +1,77 @@
-	import java.util.*;
-	import java.io.*;
-	
-	class Main {
-		private static boolean isBrace(String temp, String peek) {
-			if(temp.equals(peek)) return true;
-			return false;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+//		Stack<Integer> stack2 = new Stack<>();
+		//-1 ==> (
+		//-2 ==> {
+		String s = br.readLine();
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char temp = s.charAt(i);
+
+			if (stack.empty()) {
+				if(temp == '(') stack.add(-1);
+				else if(temp == '[') stack.add(-2);
+				else {
+					break;
+				}
+			} else {
+				if (temp == ')') {
+					if (stack.peek() == -1) {
+						stack.pop();
+						stack.add(2);
+					} else {
+						int sum = 0;
+						if(!stack.contains(-1)) {
+							stack.clear();
+							break;
+						}
+						while (stack.peek() != -1) {
+							sum += stack.pop();
+						}
+						stack.pop();
+						sum *= 2;
+						stack.add(sum);
+					}
+				} else if (temp == ']') {
+					if (stack.peek() == -2) {
+						stack.pop();
+						stack.add(3);
+					} else {
+						int sum = 0;
+						if(!stack.contains(-2)) {
+							stack.clear();
+							break;
+						}
+						while (stack.peek() != -2) {
+							sum += stack.pop();
+						}
+						stack.pop();
+						sum *= 3;
+						stack.add(sum);
+					}
+				} else {
+					if(temp == '(') stack.add(-1);
+					else if(temp == '[') stack.add(-2);
+				}
+
+			}
 		}
-		private static boolean isNumber(String peek) {
-			if(peek.equals(")") || peek.equals("]")) return false;
-			return true;
+		//System.out.println(stack);
+		while(!stack.empty()) {
+			int popNum = stack.pop();
+			if(popNum == -1 || popNum == -2 ) {
+				result = 0;
+				break;
+			}
+			else {
+				result += popNum;
+			}
 		}
-	    public static void main(String args[]) throws IOException {
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    	
-	    	String s = br.readLine();
-	    	Stack<String> stack = new Stack<>();
-	    	boolean isAble = true;
-	    	for(int i=0; i<s.length(); i++) {
-	    	
-	    		String temp = s.substring(i,i+1);
-	    		
-	    		if(temp.equals("(")) {
-	    			stack.push(")");
-	    			continue;
-	    		}
-	    		if(temp.equals("[")) {
-	    			stack.push("]");
-	    			continue;
-	    		}
-	    		int num = 0;
-	    		while(true) {
-	    			if(stack.isEmpty()) {
-	    				isAble = true;
-	    				break;
-	    			}
-	    			if(isNumber(stack.peek())) {
-	    				num += Integer.parseInt(stack.pop());
-	    			}
-	    			else {
-	    				if(isBrace(temp,stack.peek())) {
-	    					stack.pop();
-	    					int t = (")".equals(temp)) ? 2:3;
-	    					
-	    					if(num == 0) {
-	    						stack.push(String.valueOf(t));
-	    					}
-	    					else {
-	    						stack.push(String.valueOf(t*num));
-	    					}
-	    					break;
-	    				}
-	    				else {
-	    					isAble = false;
-	    					break;
-	    				}
-	    			}
-	    			
-	    		}
-	    		if(!isAble) break;
-	    	}
-	    	int result = 0;
-	    	while(!stack.isEmpty()) {
-	    		if(isNumber(stack.peek())) {
-	    			result += Integer.parseInt(stack.pop());
-	    		}
-	    		else {
-	    			isAble = false;
-	    			break;
-	    		}
-	    	}
-	    	if(isAble) bw.write(String.valueOf(result));
-	    	else bw.write(String.valueOf(0));
-	    	br.close();
-	    	bw.flush();
-	    	bw.close();
-	    }
+		System.out.println(result<0 ? 0 : result);
 	}
+}
